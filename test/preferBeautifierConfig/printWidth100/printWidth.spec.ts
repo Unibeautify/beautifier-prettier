97041d553b70236c6679bfba2f70a50e5d08e09a
Glavin Wiechert <glavin.wiechert@gmail.com>
2018-04-20 01:02:05 -0300

    Add more unit tests
    https://github.com/Unibeautify/unibeautify/issues/48

--- test/preferBeautifierConfig/printWidth100/printWidth.spec.ts
+++ test/preferBeautifierConfig/printWidth100/printWidth.spec.ts
@@ -10,45 +10,98 @@ testWithPrintWidth(80);
 testWithPrintWidth(120);
 
 function testWithPrintWidth(printWidth: number) {
-  describe(`Using .prettierrc config file with printWidth=${printWidth}`, () => {
-    test(`should successfully beautify JavaScript text ignoring Unibeautify config with printWidth=${PRETTIERRC_PRINT_WIDTH}`, () => {
-      const unibeautify = newUnibeautify();
-      unibeautify.loadBeautifier(beautifier);
+  describe(`Using .prettierrc config file with printWidth=${PRETTIERRC_PRINT_WIDTH}`, () => {
+    describe("Beautify with file path", () => {
+      test(`should successfully beautify JavaScript text ignoring Unibeautify config with printWidth=${printWidth}`, () => {
+        const unibeautify = newUnibeautify();
+        unibeautify.loadBeautifier(beautifier);
 
-      const shortString = "c";
-      const veryLongString = "c".repeat(Math.ceil(PRETTIERRC_PRINT_WIDTH / 2));
+        const shortString = "c";
+        const veryLongString = "c".repeat(Math.ceil(PRETTIERRC_PRINT_WIDTH / 2));
 
-      const shortText = `["${shortString}", "${shortString}"];\n`;
-      const shortBeautifierResult = shortText;
+        const shortText = `["${shortString}", "${shortString}"];\n`;
+        const shortBeautifierResult = shortText;
 
-      if (shortText.length > printWidth) {
-        throw new Error(
-          `Test text will always wrap. Please use a printWidth value greater than ${
-            shortText.length
-          }.`
-        );
-      }
-      const longText = `["${veryLongString}", "${veryLongString}"];\n`;
-      const longBeautifierResult = `[\n  "${veryLongString}",\n  "${veryLongString}"\n];\n`;
+        if (shortText.length > printWidth) {
+          throw new Error(
+            `Test text will always wrap. Please use a printWidth value greater than ${
+              shortText.length
+            }.`
+          );
+        }
+        const longText = `["${veryLongString}", "${veryLongString}"];\n`;
+        const longBeautifierResult = `[\n  "${veryLongString}",\n  "${veryLongString}"\n];\n`;
 
-      return Promise.all([
-        beautifyWithPrintWidth(unibeautify, shortText, printWidth).then(results => {
-          expect(results).toBe(shortBeautifierResult);
-        }),
-        beautifyWithPrintWidth(unibeautify, longText, printWidth).then(results => {
-          expect(results).toBe(longBeautifierResult);
-        })
-      ]);
+        return Promise.all([
+          beautifyWithPrintWidth({ unibeautify, text: shortText, printWidth }).then(results => {
+            expect(results).toBe(shortBeautifierResult);
+          }),
+          beautifyWithPrintWidth({ unibeautify, text: longText, printWidth }).then(results => {
+            expect(results).toBe(longBeautifierResult);
+          })
+        ]);
+      });
+    });
+    describe("Beautify without file path", () => {
+      test(`should successfully beautify JavaScript text using Unibeautify config with printWidth=${printWidth}`, () => {
+        const unibeautify = newUnibeautify();
+        unibeautify.loadBeautifier(beautifier);
+
+        const shortString = "c";
+        const veryLongString = "c".repeat(Math.ceil(printWidth / 2));
+
+        const shortText = `["${shortString}", "${shortString}"];\n`;
+        const shortBeautifierResult = shortText;
+
+        if (shortText.length > printWidth) {
+          throw new Error(
+            `Test text will always wrap. Please use a printWidth value greater than ${
+              shortText.length
+            }.`
+          );
+        }
+        const longText = `["${veryLongString}", "${veryLongString}"];\n`;
+        const longBeautifierResult = `[\n  "${veryLongString}",\n  "${veryLongString}"\n];\n`;
+
+        return Promise.all([
+          beautifyWithPrintWidth({
+            unibeautify,
+            text: shortText,
+            printWidth,
+            withFilePath: false
+          }).then(results => {
+            expect(results).toBe(shortBeautifierResult);
+          }),
+          beautifyWithPrintWidth({
+            unibeautify,
+            text: longText,
+            printWidth,
+            withFilePath: false
+          }).then(results => {
+            expect(results).toBe(longBeautifierResult);
+          })
+        ]);
+      });
     });
   });
 }
 
 const filePath: string = path.resolve(__dirname, "test.js");
 
-function beautifyWithPrintWidth(unibeautify: Unibeautify, text: string, printWidth: number) {
+function beautifyWithPrintWidth({
+  unibeautify,
+  text,
+  printWidth,
+  withFilePath = true
+}: {
+  unibeautify: Unibeautify;
+  text: string;
+  printWidth: number;
+  withFilePath?: boolean;
+}) {
   const indentSize = 2;
   return unibeautify.beautify({
-    filePath,
+    filePath: withFilePath ? filePath : undefined,
     languageName: "JavaScript",
     options: {
       JavaScript: {
